a577ec8285adedb9d298ba9cecfb876c64a38ac7
sujitmanagre <sujitmanagre2000@gmail.com>
2024-03-13 20:11:53 +0530

    initial commit

---
+++ src/test/java/com/learinig/user/mgmt/SpringUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learinig.user.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
