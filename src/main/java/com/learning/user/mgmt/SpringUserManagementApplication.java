89417f8c81c8f9ba5faded92d8301b285f43e0c6
sujitmanagre <sujitmanagre2000@gmail.com>
2024-03-23 20:31:50 +0530

    hello controller

--- src/main/java/com/learning/user/mgmt/SpringUserManagementApplication.java
+++ src/main/java/com/learning/user/mgmt/SpringUserManagementApplication.java
@@ -1,4 +1,4 @@
-package com.learinig.user.mgmt;
+package com.learning.user.mgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/learning/user/mgmt/SpringUserManagementApplicationTests.java
+++ src/test/java/com/learning/user/mgmt/SpringUserManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learinig.user.mgmt;
+package com.learning.user.mgmt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
